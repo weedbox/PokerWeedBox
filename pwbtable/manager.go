80b157b95a9d03bbdf8e0371f0a1efeb4afcb3fa
Jeffrey Yang <jeffrey@hacknext.io>
2024-03-06 14:03:21 +0800

    complete multiple table engine manager

--- pwbtable/manager.go
+++ pwbtable/manager.go
@@ -1,5 +1,14 @@
 package pwbtable
 
+import (
+	"errors"
+	"sync"
+)
+
+var (
+	ErrManagerTableNotFound = errors.New("manager: table not found")
+)
+
 type Manager interface {
 	Reset()
 
@@ -11,7 +20,6 @@ type Manager interface {
 	StartTableGame(tableID string) error
 	TableGameOpen(tableID string) error
 	UpdateBlind(tableID string, level int, ante, dealer, sb, bb int64) error
-	UpdateTablePlayers(tableID string, joinPlayers []JoinPlayer, leavePlayerIDs []string) (map[string]int, error)
 
 	// Player Table Actions
 	PlayerReserve(tableID string, joinPlayer JoinPlayer) error
@@ -29,4 +37,232 @@ type Manager interface {
 	PlayerCheck(tableID, playerID string) error
 	PlayerFold(tableID, playerID string) error
 	PlayerPass(tableID, playerID string) error
+}
+
+type manager struct {
+	tableEngines sync.Map
+}
+
+func NewManager() Manager {
+	return &manager{
+		tableEngines: sync.Map{},
+	}
+}
+
+func (m *manager) Reset() {
+	m.tableEngines.Range(func(key, value interface{}) bool {
+		_ = value.(TableEngine).CloseTable()
+		return true
+	})
+
+	m.tableEngines = sync.Map{}
+}
+
+func (m *manager) GetTableEngine(tableID string) (TableEngine, error) {
+	tableEngine, exist := m.tableEngines.Load(tableID)
+	if !exist {
+		return nil, ErrManagerTableNotFound
+	}
+	return tableEngine.(TableEngine), nil
+}
+
+func (m *manager) CreateTable(options *TableEngineOptions, callbacks *TableEngineCallbacks, setting TableSetting) (*Table, error) {
+	var engineOptions *TableEngineOptions
+	if options != nil {
+		engineOptions = options
+	} else {
+		engineOptions = NewTableEngineOptions()
+		engineOptions.Interval = 1
+	}
+
+	var engineCallbacks *TableEngineCallbacks
+	if callbacks != nil {
+		engineCallbacks = callbacks
+	} else {
+		engineCallbacks = NewTableEngineCallbacks()
+	}
+
+	gameBackend := NewNativeGameBackend()
+	tableEngine := NewTableEngine(engineOptions, WithGameBackend(gameBackend))
+	tableEngine.OnTableUpdated(engineCallbacks.OnTableUpdated)
+	tableEngine.OnTableErrorUpdated(engineCallbacks.OnTableErrorUpdated)
+	tableEngine.OnTableStateUpdated(engineCallbacks.OnTableStateUpdated)
+	tableEngine.OnTablePlayerStateUpdated(engineCallbacks.OnTablePlayerStateUpdated)
+	tableEngine.OnTablePlayerReserved(engineCallbacks.OnTablePlayerReserved)
+	tableEngine.OnGamePlayerActionUpdated(engineCallbacks.OnGamePlayerActionUpdated)
+	table, err := tableEngine.CreateTable(setting)
+	if err != nil {
+		return nil, err
+	}
+
+	m.tableEngines.Store(table.ID, tableEngine)
+	return table, nil
+}
+
+func (m *manager) PauseTable(tableID string) error {
+	tableEngine, err := m.GetTableEngine(tableID)
+	if err != nil {
+		return ErrManagerTableNotFound
+	}
+
+	return tableEngine.PauseTable()
+}
+
+func (m *manager) CloseTable(tableID string) error {
+	tableEngine, err := m.GetTableEngine(tableID)
+	if err != nil {
+		return ErrManagerTableNotFound
+	}
+
+	if err := tableEngine.CloseTable(); err != nil {
+		return err
+	}
+
+	m.tableEngines.Delete(tableID)
+	return nil
+}
+
+func (m *manager) StartTableGame(tableID string) error {
+	tableEngine, err := m.GetTableEngine(tableID)
+	if err != nil {
+		return ErrManagerTableNotFound
+	}
+
+	return tableEngine.StartTableGame()
+}
+
+func (m *manager) TableGameOpen(tableID string) error {
+	tableEngine, err := m.GetTableEngine(tableID)
+	if err != nil {
+		return ErrManagerTableNotFound
+	}
+
+	return tableEngine.TableGameOpen()
+}
+
+func (m *manager) UpdateBlind(tableID string, level int, ante, dealer, sb, bb int64) error {
+	tableEngine, err := m.GetTableEngine(tableID)
+	if err != nil {
+		return ErrManagerTableNotFound
+	}
+
+	tableEngine.UpdateBlind(level, ante, dealer, sb, bb)
+	return nil
+}
+
+func (m *manager) PlayerReserve(tableID string, joinPlayer JoinPlayer) error {
+	tableEngine, err := m.GetTableEngine(tableID)
+	if err != nil {
+		return ErrManagerTableNotFound
+	}
+
+	return tableEngine.PlayerReserve(joinPlayer)
+}
+
+func (m *manager) PlayerJoin(tableID, playerID string) error {
+	tableEngine, err := m.GetTableEngine(tableID)
+	if err != nil {
+		return ErrManagerTableNotFound
+	}
+
+	return tableEngine.PlayerJoin(playerID)
+}
+
+func (m *manager) PlayerRedeemChips(tableID string, joinPlayer JoinPlayer) error {
+	tableEngine, err := m.GetTableEngine(tableID)
+	if err != nil {
+		return ErrManagerTableNotFound
+	}
+
+	return tableEngine.PlayerRedeemChips(joinPlayer)
+}
+
+func (m *manager) PlayersLeave(tableID string, playerIDs []string) error {
+	tableEngine, err := m.GetTableEngine(tableID)
+	if err != nil {
+		return ErrManagerTableNotFound
+	}
+
+	return tableEngine.PlayersLeave(playerIDs)
+}
+
+func (m *manager) PlayerReady(tableID, playerID string) error {
+	tableEngine, err := m.GetTableEngine(tableID)
+	if err != nil {
+		return ErrManagerTableNotFound
+	}
+
+	return tableEngine.PlayerReady(playerID)
+}
+
+func (m *manager) PlayerPay(tableID, playerID string, chips int64) error {
+	tableEngine, err := m.GetTableEngine(tableID)
+	if err != nil {
+		return ErrManagerTableNotFound
+	}
+
+	return tableEngine.PlayerPay(playerID, chips)
+}
+
+func (m *manager) PlayerBet(tableID, playerID string, chips int64) error {
+	tableEngine, err := m.GetTableEngine(tableID)
+	if err != nil {
+		return ErrManagerTableNotFound
+	}
+
+	return tableEngine.PlayerBet(playerID, chips)
+}
+
+func (m *manager) PlayerRaise(tableID, playerID string, chipLevel int64) error {
+	tableEngine, err := m.GetTableEngine(tableID)
+	if err != nil {
+		return ErrManagerTableNotFound
+	}
+
+	return tableEngine.PlayerRaise(playerID, chipLevel)
+}
+
+func (m *manager) PlayerCall(tableID, playerID string) error {
+	tableEngine, err := m.GetTableEngine(tableID)
+	if err != nil {
+		return ErrManagerTableNotFound
+	}
+
+	return tableEngine.PlayerCall(playerID)
+}
+
+func (m *manager) PlayerAllin(tableID, playerID string) error {
+	tableEngine, err := m.GetTableEngine(tableID)
+	if err != nil {
+		return ErrManagerTableNotFound
+	}
+
+	return tableEngine.PlayerAllin(playerID)
+}
+
+func (m *manager) PlayerCheck(tableID, playerID string) error {
+	tableEngine, err := m.GetTableEngine(tableID)
+	if err != nil {
+		return ErrManagerTableNotFound
+	}
+
+	return tableEngine.PlayerCheck(playerID)
+}
+
+func (m *manager) PlayerFold(tableID, playerID string) error {
+	tableEngine, err := m.GetTableEngine(tableID)
+	if err != nil {
+		return ErrManagerTableNotFound
+	}
+
+	return tableEngine.PlayerFold(playerID)
+}
+
+func (m *manager) PlayerPass(tableID, playerID string) error {
+	tableEngine, err := m.GetTableEngine(tableID)
+	if err != nil {
+		return ErrManagerTableNotFound
+	}
+
+	return tableEngine.PlayerPass(playerID)
 }
