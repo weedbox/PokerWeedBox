c96a7a48a03ceadb4c750b2402837b40c33e1aa8
Jeffrey Yang <jeffrey@hacknext.io>
2024-03-07 13:18:47 +0800

    add bot & test case for bot players

---
+++ pwbtable/actor/table_engine_adapter.go
@@ -0,0 +1,95 @@
+package actor
+
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/weedbox/PokerWeedBox/pwbtable"
+	"github.com/weedbox/pokerface"
+)
+
+type tableEngineAdapter struct {
+	actor  Actor
+	engine pwbtable.TableEngine
+	table  *pwbtable.Table
+}
+
+func NewTableEngineAdapter(te pwbtable.TableEngine, table *pwbtable.Table) *tableEngineAdapter {
+
+	return &tableEngineAdapter{
+		engine: te,
+		table:  table,
+	}
+}
+
+func (tea *tableEngineAdapter) SetActor(a Actor) {
+	tea.actor = a
+}
+
+func (tea *tableEngineAdapter) UpdateTableState(tableInfo *pwbtable.Table) error {
+
+	// Clone to get a individual table structure
+	data, err := tableInfo.GetJSON()
+	if err != nil {
+		return err
+	}
+
+	var t pwbtable.Table
+	err = json.Unmarshal([]byte(data), &t)
+	if err != nil {
+		return err
+	}
+
+	tea.table = &t
+
+	return tea.actor.UpdateTableState(&t)
+}
+
+func (tea *tableEngineAdapter) GetGameState() *pokerface.GameState {
+	return tea.table.State.GameState
+}
+
+func (tea *tableEngineAdapter) GetGamePlayerIndex(playerID string) int {
+	return tea.table.GamePlayerIndex(playerID)
+}
+
+func (tea *tableEngineAdapter) Pass(playerID string) error {
+	return tea.engine.PlayerPass(playerID)
+}
+
+func (tea *tableEngineAdapter) Ready(playerID string) error {
+	return tea.engine.PlayerReady(playerID)
+}
+
+func (tea *tableEngineAdapter) Pay(playerID string, chips int64) error {
+	return tea.engine.PlayerPay(playerID, chips)
+}
+
+func (tea *tableEngineAdapter) Check(playerID string) error {
+	return tea.engine.PlayerCheck(playerID)
+}
+
+func (tea *tableEngineAdapter) Bet(playerID string, chips int64) error {
+	return tea.engine.PlayerBet(playerID, chips)
+}
+
+func (tea *tableEngineAdapter) Call(playerID string) error {
+	return tea.engine.PlayerCall(playerID)
+}
+
+func (tea *tableEngineAdapter) Fold(playerID string) error {
+	return tea.engine.PlayerFold(playerID)
+}
+
+func (tea *tableEngineAdapter) Allin(playerID string) error {
+	return tea.engine.PlayerAllin(playerID)
+}
+
+func (tea *tableEngineAdapter) Raise(playerID string, chipLevel int64) error {
+	return tea.engine.PlayerRaise(playerID, chipLevel)
+}
+
+func (tea *tableEngineAdapter) ExtendTime(playerID string, duration time.Duration) error {
+	//TODO: need to be implemented
+	return nil
+}
